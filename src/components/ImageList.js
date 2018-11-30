b0254a7d35d5b79852b2c81061a39fa5c8368ca0
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-11-29 19:16:01 -0800

    Results from calls to api are now being shown in ImageList Component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,2 +1,11 @@
 import './ImageList.css';
 import React from 'react';
+
+const ImageList = (props) => {
+    const images = props.images.map(({description, id, urls}) => {
+        return <img key={id} alt={description}src={urls.regular}/>
+    });
+    return <div>{images}</div>
+}
+
+export default ImageList;
